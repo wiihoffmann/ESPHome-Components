7f7da740a7f04da922520ce0d7da5907d69029c8
wiihoffmann <8042484+wiihoffmann@users.noreply.github.com>
2026-03-22 19:54:24 -0600

    Boilerplate done

--- Components/sump_pump_controller/__init__.py
+++ Components/sump_pump_controller/__init__.py
@@ -10,6 +10,8 @@ AUTO_LOAD = ["switch"]
 
 # Icons shown in Home Assistant. Taken from Google's MDI icon library. https://pictogrammers.com/library/mdi/
 ICON_WATER_LEVEL = "mdi:waves-arrow-up"
+ICON_BATTERY_VOLTAGE = "mdi:car-battery"
+ICON_BATTERY_CURRENT = "mdi:car-battery"
 
 # key names used in the config.yaml file for the ESPHome device
 CONF_SUMP_PUMP_CONTROLLER_ID = "sump_pump_controller_ID"
@@ -27,7 +29,11 @@ CONF_PUMP_FET_ID = "pump_fet_id"
 CONF_MANUAL_SWITCH_ID = "mode_switch_id"
 CONF_CURRENT_SENSOR_ID = "current_sensor_id"
 CONF_BATTERY_VOLTAGE_ID = "battery_voltage_id"
-
+CONF_TICK_RATE = "tick_rate"
+CONF_PUBLISH_RATE = "publish_rate"
+CONF_BATTEY_VOLTAGE_SENSOR = "battery_voltage_sensor"
+CONF_BATERY_CURRENT_SENSOR = "battery_current_sensor"
+CONF_WATER_LEVEL_SENSOR = "water_level_sensor"
 
 # All child components will extend this schema. Children which reference this SumpPumpController ID are owned by this SumpPumpController
 SUMP_PUMP_CONTROLLER_COMPONENT_SCHEMA = cv.Schema({
@@ -41,21 +47,23 @@ CONFIG_SCHEMA = cv.Schema({
         cv.Required(CONF_SUMP_DEPTH): cv.distance,
         cv.Required(CONF_HIGH_LIMIT): cv.distance,
         cv.Required(CONF_LOW_LIMIT): cv.distance,
-        cv.Required(CONF_CHARGER_CUT_IN): cv.voltage,
-        cv.Required(CONF_CHARGER_CUT_OUT): cv.voltage,
-        cv.Required(CONF_CHARGE_TIME): cv.positive_time_period_seconds,
-
         cv.Required(CONF_RED_LED_ID): cv.use_id(output.BinaryOutput),
         cv.Required(CONF_GREEN_LED_ID): cv.use_id(output.BinaryOutput),
         cv.Required(CONF_CHARGE_FET_ID): cv.use_id(output.BinaryOutput),
         cv.Required(CONF_PUMP_FET_ID): cv.use_id(output.BinaryOutput),
-        
         cv.Required(CONF_CURRENT_SENSOR_ID): cv.use_id(sensor.Sensor),
         cv.Required(CONF_BATTERY_VOLTAGE_ID): cv.use_id(sensor.Sensor),
-
         cv.Required(CONF_MANUAL_SWITCH_ID): cv.use_id(binary_sensor.BinarySensor),
-
         cv.Required(CONF_LEVEL_SENSOR_ID): cv.use_id(sensor.Sensor),
+
+        cv.Optional(CONF_CHARGER_CUT_IN, default="12.4 v"): cv.voltage,
+        cv.Optional(CONF_CHARGER_CUT_OUT, default="14.5 v"): cv.voltage,
+        cv.Optional(CONF_CHARGE_TIME, default="2 h"): cv.positive_time_period_seconds,
+        cv.Optional(CONF_PUBLISH_RATE, default="30 s"): cv.positive_time_period_seconds,
+        cv.Optional(CONF_TICK_RATE, default="1 s"): cv.positive_time_period_milliseconds,
+        cv.Optional(CONF_BATTEY_VOLTAGE_SENSOR): cv.Schema(sensor.sensor_schema(icon=ICON_BATTERY_VOLTAGE, accuracy_decimals=2, unit_of_measurement="V")),
+        cv.Optional(CONF_BATERY_CURRENT_SENSOR): cv.Schema(sensor.sensor_schema(icon=ICON_BATTERY_CURRENT, accuracy_decimals=2, unit_of_measurement="A")),
+        cv.Optional(CONF_WATER_LEVEL_SENSOR): cv.Schema(sensor.sensor_schema(icon=ICON_WATER_LEVEL, accuracy_decimals=1, unit_of_measurement="cm")),
 }).extend(cv.COMPONENT_SCHEMA)
 
 
@@ -81,4 +89,18 @@ async def to_code(config):
     pinID = await cg.get_variable(config[CONF_MANUAL_SWITCH_ID])
     cg.add(controller.setManualSwitchPin(pinID))
     levelSensor = await cg.get_variable(config[CONF_LEVEL_SENSOR_ID])
-    cg.add(controller.setSumpLevelSensor(levelSensor))
+    cg.add(controller.setDistanceSensor(levelSensor))
+    sense = await cg.get_variable(config[CONF_CURRENT_SENSOR_ID])
+    cg.add(controller.setInternalCurrentSensor(sense))
+    sense = await cg.get_variable(config[CONF_BATTERY_VOLTAGE_ID])
+    cg.add(controller.setInternalVoltageSensor(sense))
+    
+    if CONF_BATTEY_VOLTAGE_SENSOR in config:
+        sense = await sensor.new_sensor(config[CONF_BATTEY_VOLTAGE_SENSOR])
+        cg.add(controller.setDashboardVoltageSensor(sense))
+    if CONF_BATERY_CURRENT_SENSOR in config:
+        sense = await sensor.new_sensor(config[CONF_BATERY_CURRENT_SENSOR])
+        cg.add(controller.setDashboardCurrentSensor(sense))
+    if CONF_WATER_LEVEL_SENSOR in config:
+        sense = await sensor.new_sensor(config[CONF_WATER_LEVEL_SENSOR])
+        cg.add(controller.setDashboardCurrentSensor(sense))
